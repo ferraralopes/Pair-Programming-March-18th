5e29afe878b256d1e25f1237218179ecc1f082ab
JessicaGarson <ferrara.lopes@gmail.com>
2018-03-18 21:42:53 -0400

    first commit

---
+++ sample_to_be_forked.py
@@ -0,0 +1,6 @@
+dic = {'Name':'Washington', 'Song':'Breakdown','GitHub':'ferraralopes','Twitter':'washingtonlopes'}
+for index, dics in enumerate (dic):
+    print(index,'name')
+    print(index,'song')
+    print(index,'github')
+    print(index,'twitter')
